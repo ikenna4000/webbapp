627caa9d780a62f80bcf5cd9d50ec05c68e38ede
ikenna4000 <ikenna4000@yahoo.com>
2023-12-20 18:47:34 +0000

    adding to commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>App Deployment</title>
+    </head>
+    <body>
+    <h1>My Web App Deployment</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quisquam assumenda consequuntur illum soluta maiores minus repellendus itaque. Beatae repellendus quibusdam vero aliquid illo quam ullam minima blanditiis iste expedita.</p>
+    <i>I Love You</i>    
+</body>
+
+</html>

48a2fd5d896b90b5d24e235d7b600326ba244afd
ikenna4000 <ikenna4000@yahoo.com>
2023-12-20 18:57:11 +0000

    adding to ik for gala

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <body>
     <h1>My Web App Deployment</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quisquam assumenda consequuntur illum soluta maiores minus repellendus itaque. Beatae repellendus quibusdam vero aliquid illo quam ullam minima blanditiis iste expedita.</p>
-    <i>I Love You</i>    
+    <i>I Love You</i> 
+    <h1> reloading IK for gala</h1>
 </body>
 
 </html>
